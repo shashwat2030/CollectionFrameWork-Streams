c4895356a651376c6aa8c77d5c73617e0e667591
shashwat2030 <75948019+shashwat2030@users.noreply.github.com>
2025-06-16 10:35:19 +0530

    Add 8_Jun _2025 folder to its branch

--- 9_Jun_2025/Map/CSLMap.java
+++
@@ -1,9 +0,0 @@
-import java.util.concurrent.ConcurrentSkipListMap;
-
-public class CSLMap {
-    public static void exe(){
-        ConcurrentSkipListMap<Integer,String>cslm=new ConcurrentSkipListMap<>();
-        cslm.put(2,"Microsoft");
-    }
-    public static void main(String[] ShashwatKumarChoudhary){exe();}
-}
