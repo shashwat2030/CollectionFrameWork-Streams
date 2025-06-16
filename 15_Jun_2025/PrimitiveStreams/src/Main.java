763e4438e63ff818fa99ba2ca50956d69ddb2feb
shashwat2030 <75948019+shashwat2030@users.noreply.github.com>
2025-06-16 10:04:59 +0530

    Saving All files and folders

---
+++ 15_Jun_2025/PrimitiveStreams/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void exe() {
+        //System.out.println("नमस्कार ");
+        PrimitiveStreams.primitiveStreams();
+    }
+
+    public static void main(String[] args) {
+        exe();
+
+    }
+}
