4636c6167f546c3c00dd64cb93664634b9021ca4
shashwat2030 <75948019+shashwat2030@users.noreply.github.com>
2025-06-16 10:55:36 +0530

    Add 15_Jun_2025 folder to its branch

--- 15_Jun_2025/PrimitiveStreams/src/Main.java
+++ 15_Jun_2025/PrimitiveStreams/src/Main.java
@@ -1,10 +1,11 @@
 public class Main {
     public static void exe() {
-        IC.ic();
-
+        //System.out.println("नमस्कार ");
+        PrimitiveStreams.primitiveStreams();
     }
 
     public static void main(String[] args) {
         exe();
+
     }
 }
